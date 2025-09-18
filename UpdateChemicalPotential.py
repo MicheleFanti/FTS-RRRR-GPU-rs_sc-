adb97099ab24462fa37a1de4afe1351e5046f2a3
MicheleFanti <94638941+MicheleFanti@users.noreply.github.com>
2025-09-18 18:19:10 +0200

    Update UpdateChemicalPotential.py

--- UpdateChemicalPotential.py
+++ UpdateChemicalPotential.py
@@ -38,22 +38,16 @@ class SCFTUpdater:
         for species in rho_bb_new:
             comp_vchi_ps, comp_vchi_pp = np.zeros(gridshape[:2]), np.zeros(gridshape[:2])
             if species != "pb":
-                '''
-                h_as_total = np.zeros_like(rhoPnew)
-                for a_key in h_as:
-                    idx_in_res = next((i for i,res in enumerate(self.species_order) if res==species), None)
-                    if idx_in_res is not None:
-                        h_as_total += self.hydro_lambdas[a_key]*self.hydro_charges[a_key][idx_in_res]*ihath_a[a_key]'''
                 comp_vchi_ps = rhoS_total*self.vchi_ps
                 comp_vchi_pp = rhoPnew*self.vchi_pp
-            wP_trial[species] = np.broadcast_to(comp_vchi_ps[..., None] + comp_vchi_pp[..., None] , gridshape).copy()
+            wP_trial[species] = np.broadcast_to(comp_vchi_ps[..., None] + comp_vchi_pp[..., None] , gridshape).copy()+np.broadcast_to(xi[..., None], gridshape).copy()
             wP_trial[species] -= w_prior_bb[species]
 
         wSc_trial = {} 
         for sc in rho_sc_new:
             contrib_rhoS = rhoS_total * self.vchi_ps
             contrib_hb = -0.5*self.epsilon_hb*np.fft.ifft2(np.fft.fft2(np.tensordot(rho_sc_new['Csc' if sc=='Nsc' else 'Nsc']*ang_weights[None,None,:], A_hb, axes=([2],[1])), axes=(0,1))*K_hb[..., None], axes=(0,1)).real*(dx**2)
-            wSc_trial[sc] = (contrib_hb + np.broadcast_to(contrib_rhoS[..., None], gridshape).copy())
+            wSc_trial[sc] = (contrib_hb + np.broadcast_to(contrib_rhoS[..., None], gridshape).copy()+np.broadcast_to(xi[..., None], gridshape).copy())
             wSc_trial[sc] -= w_prior_sc[sc]
             
         WScRs_trial = {}
@@ -69,27 +63,27 @@ class SCFTUpdater:
             comp_vchi_pp = rhoPnew*self.vchi_pp
             comp_h_as = h_as_total
             comp_es = self.bjerrum_length* self.es_charges[species]*ihatc
-            WScRs_trial[species] = np.broadcast_to(comp_vchi_pp[..., None]+ comp_vchi_ps[..., None] + comp_h_as[..., None] + comp_es[..., None], gridshape).copy()
+            WScRs_trial[species] = np.broadcast_to(comp_vchi_pp[..., None]+ comp_vchi_ps[..., None] + comp_h_as[..., None] + comp_es[..., None] +np.broadcast_to(xi[..., None], gridshape).copy(), gridshape).copy()
             WScRs_trial[species] -= w_prior_sc_rs[species]
 
         wS_trial = {}
         for s in rho_solv_new:
             contrib_rhoP = rhoPnew * self.vchi_ps
             contrib_es = self.es_charges[s] * ihatc
-            wS_trial[s] = gamma*(contrib_rhoP + contrib_es)
+            wS_trial[s] = (contrib_rhoP + contrib_es+ xi)
 
         w_new_bb = {}
         for key in w_prior_bb:
-            w_new_bb[key] = w_prior_bb[key] + gamma * (wP_trial[key] +np.broadcast_to(xi[..., None], gridshape).copy())
+            w_new_bb[key] = w_prior_bb[key] + gamma * (wP_trial[key])
         w_new_solv = {}
         for key in w_prior_solv:
-            w_new_solv[key] = w_prior_solv[key] + gamma *( wS_trial[key] + xi)
+            w_new_solv[key] = w_prior_solv[key] + gamma *( wS_trial[key] )
         w_new_sc = {}
         for key in w_prior_sc:
-            w_new_sc[key] = w_prior_sc[key] + gamma*(wSc_trial[key] +np.broadcast_to(xi[..., None], gridshape).copy())
+            w_new_sc[key] = w_prior_sc[key] + gamma*(wSc_trial[key] )
         w_new_sc_rs = {}
         for key in WScRs_trial:
-            w_new_sc_rs[key] = w_prior_sc_rs[key] + gamma*(WScRs_trial[key] +np.broadcast_to(xi[..., None], gridshape).copy())
+            w_new_sc_rs[key] = w_prior_sc_rs[key] + gamma*(WScRs_trial[key])
 
         bb_deviation = sum(np.sum((wP_trial[key])**2 * spat_weights[..., None]*ang_weights[None, None, ...]) for key in wP_trial)
         sc_rs_deviation = sum(np.sum((WScRs_trial[key])**2 * spat_weights[..., None]*ang_weights[None, None, ...]) for key in WScRs_trial)
